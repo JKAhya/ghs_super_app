6cf7f460c959b002523260e406e93b5faee345d4
Jung-Hyeon Kim <121010242+naon1735@users.noreply.github.com>
2025-03-22 01:54:28 +0900

    Skeleton(By 0409)

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+{@render children()}
+
+
+<div class="dock dock-xl px-">
+    <button>
+      <span class="icon size-[1.2em]">🏫</span>
+      <span class="dock-label">학교</span>
+    </button>
+    <button>
+      <span class="icon size-[1.2em]">🚇</span>
+      <span class="dock-label">등교</span>
+    </button>
+    <!--<button class="dock-active">-->
+    <button>
+      <span class="icon size-[1.2em]">🏠</span>
+      <span class="dock-label">홈</span>
+    </button>
+    <button>
+      <span class="icon size-[1.2em]">🤩</span>
+      <span class="dock-label">일상</span>
+    </button>
+    <button>
+      <span class="icon size-[1.2em]">📑</span>
+      <span class="dock-label">전체</span>
+    </button>
+</div>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,21 @@
+<div class="flex flex-col md:flex-row h-screen">
+  <!-- 왼쪽 영역: 로그인 텍스트 -->
+  <div class="md:w-1/2 flex items-center justify-center p-8">
+    <div>
+      <h1 class="text-4xl font-bold">로그인</h1>
+    </div>
+  </div>
+
+  <!-- 오른쪽 영역: 로그인 폼 -->
+  <div class="md:w-1/2 flex items-center justify-center p-8">
+    <fieldset class="fieldset w-full max-w-sm bg-base-200 border border-base-300 p-4 rounded-box items-center justify-center">
+      <label class="fieldset-label items-center justify-center">Email</label>
+      <input type="email" class="input" placeholder="Email" />
+
+      <label class="fieldset-label mt-4 items-center justify-center">Password</label>
+      <input type="password" class="input" placeholder="Password" />
+
+      <button class="btn btn-neutral mt-4">Login</button>
+    </fieldset>
+  </div>
+</div>
